77f6fb6a0764202e9de313da13afbbdb0f4e0406
johnnyzhang <1113638674@qq.com>
2016-12-13 12:52:23 +0800

    init

---
+++ system/config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+$config['charset'] = 'utf8';
+$config['prefix'] = 'aws_';
+$config['driver'] = 'MySQLi';
+$config['master'] = array (
+  'charset' => 'utf8',
+  'host' => 'localhost',
+  'username' => 'root',
+  'password' => 'ab199255',
+  'dbname' => 'ireply',
+);
+$config['slave'] = false;
